704695b388cf9d6c0b6965ce5755079d3c1ceaae
Richard Wei <rxrwei@gmail.com>
2017-06-06 21:32:12 +0800

    Bump version for Swift 4

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "Parsey",
     dependencies: [
-        .Package(url: "https://github.com/rxwei/Funky", majorVersion: 1)
+        .Package(url: "https://github.com/rxwei/Funky", majorVersion: 2)
     ]
 )
